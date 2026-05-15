ca331ce8e97b287d1bcbe1d8572232b864e4c254
hamster1198 <32123210+pangqil@users.noreply.github.com>
2026-05-15 09:24:15 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long answer = -1;
+    long long sum=0;
+    for(int i=1;i<=count;i++){
+        sum+=i*price;
+
+    }
+    
+    if(sum>money){
+        answer=sum-money;
+    }
+    else{
+        answer=0;
+    }
+    return answer;
+}
