834e146566732a8a83340b01717cdaa6d96723f6
hamster1198 <32123210+pangqil@users.noreply.github.com>
2026-05-29 12:34:28 +0900

    [level 2] Title: 시소 짝꿍, Time: 5191.71 ms, Memory: 6.56 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+     sort(weights.begin(), weights.end());
+    //1:1 1:2 3:2 4:3
+    for (int i=0;i<weights.size();i++)
+    {
+        for (int j=i+1;j<weights.size();j++)
+        {
+            if (weights[i]==weights[j])
+            {
+                answer++;
+            }
+            else if (weights[i]*2==weights[j])
+            {
+                answer++;
+            }
+            else if (weights[i]*3==weights[j]*2)
+            {
+                answer++;
+            }
+            else if (weights[i]*4==weights[j]*3)
+            {
+                answer++;
+            }
+        }
+    }
+    
+    
+    return answer;
+}
