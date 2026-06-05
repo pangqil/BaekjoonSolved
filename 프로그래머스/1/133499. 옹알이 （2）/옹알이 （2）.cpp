c711388312658128eed658f28f97a97a00830d0a
hamster1198 <32123210+pangqil@users.noreply.github.com>
2026-06-05 15:49:02 +0900

    [level 1] Title: 옹알이 (2), Time: 0.03 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -4,32 +4,24 @@ using namespace std;
 
 int solution(vector<string> babbling) {
     vector<string> sounds = {"aya", "ye", "woo", "ma"};
-    int answer = 0;
-
-    for (string word : babbling) {
-        string prev = "";
-        bool ok = true;
-
-        while (!word.empty()) {
-            bool matched = false;
-
-            for (string s : sounds) {
-                if (word.rfind(s, 0) == 0 && s != prev) {
-                    word.erase(0, s.size());
-                    prev = s;
-                    matched = true;
-                    break;
-                }
-            }
-
-            if (!matched) {
-                ok = false;
-                break;
+    int answer=0;
+    
+    for (string word:babbling)
+    {
+        int preidx=-1;
+        for (int i=0;i<sounds.size();i++)
+        {
+            if (word.substr(0, sounds[i].length())==sounds[i]&&preidx!=i)
+            {
+                word.erase(0, sounds[i].length());
+                preidx=i;
+                i=-1;
             }
         }
-
-        if (ok) answer++;
+       
+        if (word.size()==0) answer++;
+        preidx=-1;
     }
-
+    
     return answer;
-}
+}//ayaayaa -> aya, yee, u, maa
